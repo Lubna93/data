4b0e63f690cd0c52e6502710631500b45a812061
Lubna Altungi <lubna.altungi@gmail.com>
2024-10-14 10:21:39 +0200

    search

--- src/Controller/DataverseController.php
+++ src/Controller/DataverseController.php
@@ -1,36 +1,5 @@
 <?php
 
-// namespace App\Controller;
-
-// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\Response;
-// use Symfony\Component\Routing\Attribute\Route;
-// use App\Service\DataverseService;
-
-// class DataverseController extends AbstractController
-// {
-//     private $dataverseService;
-
-//     public function __construct(DataverseService $dataverseService)
-//     {
-//         $this->dataverseService = $dataverseService;
-//     }
-
-//     #[Route('/datasets', name: 'datasets')]
-//     public function showDatasets(): Response
-//     {
-//         $response = $this->dataverseService->getLastDatasets();
-
-//         // Ensure you get the datasets from the response
-//         $datasets = $response['data']['items'];
-
-//         return $this->render('dataverse/datasets.html.twig', [
-//             'datasets' => $datasets,
-//         ]);
-//     }
-// }
-
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -64,7 +33,7 @@ class DataverseController extends AbstractController
     #[Route('/search-datasets', name: 'search_datasets')]
     public function searchDatasets(Request $request): Response
     {
-        $query = $request->query->get('query', 'cnrs'); // default to 'cnrs' if no query
+        $query = $request->query->get('query', 'univ-ch'); 
 
         $response = $this->dataverseService->searchDatasets($query);
 
@@ -76,4 +45,112 @@ class DataverseController extends AbstractController
             'query' => $query,
         ]);
     }
+
+
+
+
+#[Route('/advanced-search', name: 'advanced_search')]
+public function advancedSearch(Request $request, DataverseService $dataverseService): Response
+{
+    $title = $request->query->get('title', '');
+    $subtitle = $request->query->get('subtitle', '');
+    $author = $request->query->get('authorName', '');
+    $keywordValue = $request->query->get('keywordValue', '');
+    $kindOfData = $request->query->get('kindOfData', '');
+    $seriesName = $request->query->get('seriesName', '');
+    $subject = $request->query->get('subject', '');
+    $producerName = $request->query->get('producerName', '');
+    $producerAffiliation = $request->query->get('producerAffiliation', '');
+    $distributorName = $request->query->get('distributorName', '');
+    $description = $request->query->get('description', '');
+    $authorIdentifierScheme = $request->query->get('authorIdentifierScheme', '');
+    $dataOrigin = $request->query->get('dataOrigin', '');
+    $lifeCycleStep = $request->query->get('lifeCycleStep', '');
+    $dsDescriptionLanguage = $request->query->get('dsDescriptionLanguage', '');
+    $language = $request->query->get('language', '');
+    $contributorName = $request->query->get('contributorName', '');
+    $productionPlace = $request->query->get('productionPlace', '');
+    $productionDate = $request->query->get('productionDate', '');
+    $distributionDate = $request->query->get('distributionDate', '');
+    $dsPublicationDate = $request->query->get('dsPublicationDate', '');
+    $relatedDatasetIDType = $request->query->get('relatedDatasetIDType', ''); // Updated to handle a single value
+
+    $response = $dataverseService->searchDatasetsAdvanced(
+        $title, $subtitle, $author, $keywordValue, $kindOfData, $seriesName, $subject, 
+        $producerName, $producerAffiliation, $distributorName, $description, 
+        $authorIdentifierScheme, $dataOrigin, $lifeCycleStep, $dsDescriptionLanguage, $language,
+        $contributorName, $productionPlace, $productionDate, 
+        $distributionDate, $dsPublicationDate, $relatedDatasetIDType
+    );
+
+    $datasets = $response['data']['items'] ?? [];
+
+    return $this->render('dataverse/advanced_search.html.twig', [
+        'datasets' => $datasets,
+        'search_params' => compact(
+            'title', 'subtitle', 'author', 'keywordValue', 'kindOfData', 'seriesName', 'subject', 
+            'producerName', 'producerAffiliation', 'distributorName', 'description', 
+            'authorIdentifierScheme', 'dataOrigin', 'lifeCycleStep', 'dsDescriptionLanguage', 'language',
+            'contributorName', 'productionPlace', 'productionDate', 
+            'distributionDate', 'dsPublicationDate', 'relatedDatasetIDType'
+        ),
+    ]);
+}
+
+
+#[Route('/results-search', name: 'results_search')]
+public function resultsSearch(Request $request, DataverseService $dataverseService): Response
+{
+    $title = $request->query->get('title', '');
+    $subtitle = $request->query->get('subtitle', '');
+    $alternativeURL = $request->query->get('alternativeURL', '');
+    $author = $request->query->get('authorName', '');
+    $keywordValue = $request->query->get('keywordValue', '');
+    $kindOfData = $request->query->get('kindOfData', '');
+    $seriesName = $request->query->get('seriesName', '');
+    $subject = $request->query->get('subject', '');
+    $producerName = $request->query->get('producerName', '');
+    $producerAffiliation = $request->query->get('producerAffiliation', '');
+    $distributorName = $request->query->get('distributorName', '');
+    $description = $request->query->get('description', '');
+    $authorIdentifierScheme = $request->query->get('authorIdentifierScheme', '');
+    $dataOrigin = $request->query->get('dataOrigin', '');
+    $lifeCycleStep = $request->query->get('lifeCycleStep', '');
+    $dsDescriptionLanguage = $request->query->get('dsDescriptionLanguage', '');
+    $language = $request->query->get('language', '');
+    $contributorName = $request->query->get('contributorName', '');
+    $productionPlace = $request->query->get('productionPlace', '');
+    $productionDate = $request->query->get('productionDate', '');
+    $distributionDate = $request->query->get('distributionDate', '');
+    $dsPublicationDate = $request->query->get('dsPublicationDate', '');
+    $relatedDatasetIDType = $request->query->get('relatedDatasetIDType', ''); // Updated to handle a single value
+
+    $response = $dataverseService->searchDatasetsAdvanced(
+        $title, $subtitle, $author, $alternativeURL, $keywordValue, $kindOfData, $seriesName, $subject, 
+        $producerName, $producerAffiliation, $distributorName, $description, 
+        $authorIdentifierScheme, $dataOrigin, $lifeCycleStep, $dsDescriptionLanguage, $language,
+        $contributorName, $productionPlace, $productionDate, 
+        $distributionDate, $dsPublicationDate, $relatedDatasetIDType
+    );
+
+    $datasets = $response['data']['items'] ?? [];
+
+    return $this->render('dataverse/results_search.html.twig', [
+        'datasets' => $datasets,
+        'search_params' => compact(
+            'title', 'subtitle', 'alternativeURL', 'author', 'keywordValue', 'kindOfData', 'seriesName', 'subject', 
+            'producerName', 'producerAffiliation', 'distributorName', 'description', 
+            'authorIdentifierScheme', 'dataOrigin', 'lifeCycleStep', 'dsDescriptionLanguage', 'language',
+            'contributorName', 'productionPlace', 'productionDate', 
+            'distributionDate', 'dsPublicationDate', 'relatedDatasetIDType'
+        ),
+    ]);
+}
+
+
+
+
+
+
+
 }
--- src/Service/DataverseService.php
+++ src/Service/DataverseService.php
@@ -1,37 +1,4 @@
 <?php
-// namespace App\Service;
-
-// use Symfony\Contracts\HttpClient\HttpClientInterface;
-
-// class DataverseService
-// {
-//     private $client;
-
-//     public function __construct(HttpClientInterface $client)
-//     {
-//         $this->client = $client;
-//     }
-
-//     public function getDataset($datasetId)
-//     {
-//         $response = $this->client->request(
-//             'GET',
-//             'datasets/' . $datasetId
-//         );
-
-//         return $response->toArray();
-//     }
-
-//     public function getLastDatasets()
-//     {
-//         $response = $this->client->request(
-//             'GET',
-//             'search?q=cnrs&type=dataset'
-//         );
-
-//         return $response->toArray();
-//     }
-// }
 
 namespace App\Service;
 
@@ -60,7 +27,8 @@ class DataverseService
     {
         $response = $this->client->request(
             'GET',
-            'search?q=cnrs&type=dataset'
+            // 'search?q=cnrs&type=dataset&subtree=cirad'
+            'search?q=univ-ch&type=dataset&subtree=univ-ch'
         );
 
         return $response->toArray();
@@ -70,9 +38,115 @@ class DataverseService
     {
         $response = $this->client->request(
             'GET',
-            'search?q=' . urlencode($query) . '&type=dataset'
+            // 'search?q=' . urlencode($query) . '&type=dataset' . '&subtree=cirad'
+            'search?q=' . urlencode($query) . '&type=dataset' . '&subtree=univ-ch'
         );
 
         return $response->toArray();
     }
+
+
+
+
+public function searchDatasetsAdvanced(
+    string $title = '', string $subtitle = '', string $alternativeURL = '', string $author = '', string $keywordValue = '', 
+    string $kindOfData = '', string $seriesName= '', string $subject = '', string $language = '', string $dsDescriptionLanguage = '', string $producerName = '', string $producerAffiliation = '',
+    string $distributorName = '', string $description = '', string $authorIdentifierScheme = '', 
+    string $dataOrigin = '', string $lifeCycleStep = '', string $contributorName = '', 
+    string $productionPlace = '', string $productionDate = '', string $distributionDate = '',
+    string $dsPublicationDate = '', string $relatedDatasetIDType = ''
+)
+{
+    $conditions = [];
+
+    if ($title) {
+        $conditions[] = "title:" . urlencode($title);
+    }
+    if ($subtitle) {
+        $conditions[] = "subtitle:" . urlencode($subtitle);
+    }
+    if ($alternativeURL) {
+        $conditions[] = "alternativeURL:" . urlencode($alternativeURL);
+    }
+    if ($author) {
+        $conditions[] = "authorName:" . urlencode($author);
+    }
+
+    if ($keywordValue) {
+        $conditions[] = "keywordValue_ss:%22" . urlencode($keywordValue) . "%22";
+    }
+
+    if ($kindOfData) {
+        $conditions[] = "kindOfData:%22" . urlencode($kindOfData) . "%22";
+    }
+    if ($seriesName) {
+        $conditions[] = "seriesName:%22" . urlencode($seriesName) . "%22";
+    }
+    
+    if ($subject) {
+        $conditions[] = "subject:%22" . urlencode($subject) . "%22";
+    }
+    if ($language) {
+        $conditions[] = "language:%22" . urlencode($language) . "%22";
+    } 
+    if ($dsDescriptionLanguage) {
+        $conditions[] = "dsDescriptionLanguage:%22" . urlencode($dsDescriptionLanguage) . "%22";
+    } 
+    if ($producerName) {
+        $conditions[] = "producerName:%22" . urlencode($producerName) . "%22";
+    } 
+    if ($producerAffiliation) {
+        $conditions[] = "producerAffiliation:%22" . urlencode($producerAffiliation) . "%22";
+    }
+    if ($distributorName) {
+        $conditions[] = "distributorName:%22" . urlencode($distributorName) . "%22";
+    }
+
+    if ($description) {
+        $conditions[] = "dsDescriptionValue:%22" . urlencode($description) . "%22";
+    }
+
+    if ($authorIdentifierScheme) {
+        $conditions[] = "authorIdentifierScheme:%22" . urlencode($authorIdentifierScheme) . "%22";
+    }
+
+    if ($dataOrigin) {
+        $conditions[] = "dataOrigin:%22" . urlencode($dataOrigin) . "%22";
+    }
+
+    if ($lifeCycleStep) {
+        $conditions[] = "lifeCycleStep:%22" . urlencode($lifeCycleStep) . "%22";
+    }
+
+    if ($contributorName) {
+        $conditions[] = "contributorName:%22" . urlencode($contributorName) . "%22";
+    }
+
+    if ($productionPlace) {
+        $conditions[] = "productionPlace:%22" . urlencode($productionPlace) . "%22";
+    }
+
+    if ($productionDate) {
+        $conditions[] = "productionDate:%22" . urlencode($productionDate) . "%22";
+    }
+
+    if ($distributionDate) {
+        $conditions[] = "distributionDate:%22" . urlencode($distributionDate) . "%22";
+    }
+
+    if ($dsPublicationDate) {
+        $conditions[] = "dsPublicationDate:%22" . urlencode($dsPublicationDate) . "%22";
+    }
+
+    if ($relatedDatasetIDType) {
+        $conditions[] = "relatedDatasetIDType:%22" . urlencode($relatedDatasetIDType) . "%22";
+    }
+
+    $query = "search?q=" . implode(" AND ", $conditions);
+    $response = $this->client->request('GET', $query . '&type=dataset&subtree=univ-ch');
+    return $response->toArray();
+}
+
+
+
 }
